f057161f87945712c132e02ef7e6ac3804a48dd3
Kevinzhou2016 <kzhou2017@outlook.com>
2025-05-13 12:12:01 +0800

    bug fix

--- Src/main.py
+++ Src/main.py
@@ -4,6 +4,7 @@ if __name__ == "__main__":
     auth="XXX"
     taskConfiguration_dabaseid='XXXXX'
     databaseid='XXX'
+    offDayDatabaseId='XXX'
     timeDeltaWithUTC=8
-    knotion=RecurringTask()
-    knotion.process(auth,taskConfiguration_dabaseid,databaseid,timeDeltaWithUTC)
+    knotion=RecurringTask()    
+    knotion.process(auth,taskConfiguration_dabaseid,offDayDatabaseId,databaseid,timeDeltaWithUTC)
